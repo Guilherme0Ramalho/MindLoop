208d117c7970c405e843bb8eb9f2fe75184cd516
Guilherme0Ramalho <guilherme0110ramalho@gmail.com>
2025-09-02 18:23:24 -0300

    projeto MindLoop

---
+++ src/components/ItemSuggestion.tsx
@@ -0,0 +1,12 @@
+import  type {ComponentProps} from 'react'
+
+
+type ItemSuggestionProps =    ComponentProps<'button'> &{
+    title: string
+}
+
+export function ItemSuggestion  ({title, ...props}: ItemSuggestionProps) {
+    return (
+        <button {...props}>{title}</button>
+    )
+}
