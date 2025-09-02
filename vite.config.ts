be738bdc6b130681e2fc3e3db9100304ea189447
Guilherme0Ramalho <guilherme0110ramalho@gmail.com>
2025-09-02 18:56:58 -0300

    arrumando hospedagem

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+
+// https://vite.dev/config/
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MindLoop/', // 👈 importante para o GitHub Pages
 })
+
